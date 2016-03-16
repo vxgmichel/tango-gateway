c60fd387d7a7a2bf3740b5bd27b495c6323b716f
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-03-16 14:39:24 +0100

    Refactor

---
+++ giop.py
@@ -0,0 +1,156 @@
+"""Provide helpers to pack and unpack GIOP message."""
+
+import struct
+from enum import IntEnum
+from collections import namedtuple
+
+# Constants
+
+MAGIC_GIOP = b'GIOP'
+MAGIC_IOR = b'IOR:'
+LITTLE_ENDIAN = 1
+GIOP_HEADER_STRUCT = '4sBBBBI'
+REPLY_HEADER_STRUCT = 'III'
+IOR_STRUCT_1 = 'II'
+IOR_STRUCT_2 = IOR_STRUCT_1 + '{:d}sIIIBBI'
+IOR_STRUCT_3 = IOR_STRUCT_2 + '{:d}sH0I'
+IOR_LENGTH_STRUCT = 'BBI{}sH0I'
+MIN_IOR_LENGTH = 76
+HEXA_DIGIT_SET = set(b'0123456789abcdef')
+
+
+# Enumerations
+
+class MessageType(IntEnum):
+    Request = 0
+    Reply = 1
+    CancelRequest = 2
+    LocateRequest = 3
+    LocateReply = 4
+    CloseConnection = 5
+    MessageError = 6
+    Fragment = 7
+
+
+class ReplyStatus(IntEnum):
+    NoException = 0
+    UserException = 1
+    SystemException = 2
+    LocationForward = 3
+
+
+# Structures
+
+GiopHeader = namedtuple(
+    'GiopHeader',
+    'giop major minor order message_type size')
+
+ReplyHeader = namedtuple(
+    'ReplyHeader',
+    'service_context request_id reply_status')
+
+IOR = namedtuple(
+    'IOR',
+    'first dtype_length dtype nb_profile tag '
+    'length major minor host_length host port body')
+
+
+# ASCII/bytes helpers
+
+def ascii_to_bytes(s):
+    return bytearray(int(s[i:i+2], 16) for i in range(0, len(s), 2))
+
+
+def bytes_to_ascii(s):
+    return b''.join(format(x, '02x').encode() for x in s)
+
+
+# GIOP helpers
+
+def unpack_giop_header(bytes_header):
+    values = struct.unpack(GIOP_HEADER_STRUCT, bytes_header)
+    header = GiopHeader(*values)
+    assert header.giop == MAGIC_GIOP
+    assert header.order == LITTLE_ENDIAN
+    assert header.major == 1
+    assert header.minor in (0, 1)
+    MessageType(header.message_type)
+    return header
+
+
+def pack_giop(header, body):
+    values = (MAGIC_GIOP,) + header[1:-1] + (len(body),)
+    bytes_header = struct.pack(GIOP_HEADER_STRUCT, *values)
+    return bytes_header + body
+
+
+# Reply helpers
+
+def unpack_reply_header(bytes_header):
+    values = struct.unpack(REPLY_HEADER_STRUCT, bytes_header)
+    header = ReplyHeader(*values)
+    ReplyStatus(header.reply_status)
+    return header
+
+
+def pack_reply(header, body):
+    bytes_header = struct.pack(REPLY_HEADER_STRUCT, *header)
+    return bytes_header + body
+
+
+# IOR helpers
+
+def valid_ior(string):
+    if len(string) < MIN_IOR_LENGTH or not string.startswith(MAGIC_IOR):
+        return False
+    return not set(string[4:]) - HEXA_DIGIT_SET
+
+
+def find_ior(body, index=4):
+    while True:
+        index = body.find(MAGIC_IOR, index)
+        if index < 0:
+            return False
+        size = struct.unpack('I', body[index-4:index]) - 1
+        ior = body[index:index+size]
+        if len(ior) == size and valid_ior(ior):
+            start = index - 4
+            stop = index + struct.calcsize("{:d}s0I".format(size))
+            return unmarshal_ior(ior), start, stop
+        index += 1
+
+
+def repack_ior(body, ior, start, stop):
+    string = marshal_ior(ior) + b'\x00'
+    form = 'I{:d}s0I'.format(len(string))
+    ior_struct = struct.pack(form, len(string), string)
+    return body[:start] + ior_struct + body[stop:]
+
+
+def unmarshal_ior(encoded_ior):
+    assert encoded_ior[:4] == MAGIC_IOR
+    ior = ascii_to_bytes(encoded_ior[4:])
+    dtype_length = struct.unpack_from(IOR_STRUCT_1, ior)[-1]
+    form = IOR_STRUCT_2.format(dtype_length)
+    host_length = struct.unpack_from(form, ior)[-1]
+    form = IOR_STRUCT_3.format(dtype_length, host_length)
+    values = struct.unpack_from(form, ior)
+    values += (ior[struct.calcsize(form):],)
+    return IOR(*values)
+
+
+def marshal_ior(ior):
+    ior = update_ior_length(ior)
+    form = IOR_STRUCT_3.format(ior.dtype_length, ior.host_length)
+    string = struct.pack(form, *ior[:-1])
+    string += ior.body
+    return MAGIC_IOR + bytes_to_ascii(string)
+
+
+def update_ior_length(ior):
+    d = ior._asdict()
+    d['host_length'] = len(ior.host)
+    d['dtype_length'] = len(ior.dtype)
+    form = IOR_LENGTH_STRUCT.format(len(ior.host))
+    d['length'] = struct.calcsize(form) + len(ior.body)
+    return IOR(**d)
--- sniffer.py
+++ sniffer.py
@@ -1,156 +1,105 @@
+"""Provide a Tango gateway server."""
+
+import giop
 import asyncio
-import enum
-import struct
-import ior_decoder
 from functools import partial
-
-TANGO_HOST = "localhost:10000"
-
-MESSAGE_TYPE = ["Request", "Reply", "CancelRequest", "LocateRequest",
-                "LocateReply", "CloseConnection", "MessageError", "Fragment"]
-
-REPLY_STATUS_TYPE = ["NO_EXCEPTION,"
-                     "USER_EXCEPTION",
-                     "SYSTEM_EXCEPTION",
-                     "LOCATION_FORWARD"]
-
-IOR_TOKEN = b'IOR'
-
-
-
-NETWORK_HOST = "194.47.253.112"
-
-
-def ascii_to_octet(s):
-    return bytes(int(s[i:i + 2], 16) for i in range(0, len(s), 2))
+from contextlib import closing
 
 
 @asyncio.coroutine
-def basic_pipe(reader, writer, message):
-    while not reader.at_eof():
-        data = yield from reader.read(4)
-        print(message, ":", data)
-        writer.write(data)
-    writer.close()
+def forward_pipe(reader, writer):
+    with closing(writer):
+        while not reader.at_eof():
+            data = yield from reader.read(4096)
+            writer.write(data)
+
 
 @asyncio.coroutine
-def subscribed_handler(hots, port , server_reader, server_writer):
-    client_reader, client_writer =yield from asyncio.open_connection(hots, port, loop=loop)
-    task1 = basic_pipe(server_reader, client_writer, "--------client to db")
-    task2 = basic_pipe(client_reader, server_writer, "---------db to client")
+def forward(s_reader, s_writer, host, port):
+    c_reader, c_writer = yield from asyncio.open_connection(host, port)
+    task1 = forward_pipe(s_reader, c_writer)
+    task2 = forward_pipe(c_reader, s_writer)
     yield from asyncio.gather(task1, task2)
-    print("Close ok")
-
-@asyncio.coroutine
-def handle_echo(hots, port , reader, writer):
-    data = yield from reader.read(100)
-    message = data.decode()
-    addr = writer.get_extra_info('peername')
-    print("Received %r from %r" % (message, addr))
-
-    print("Send: %r" % message)
-    writer.write(data)
-    yield from writer.drain()
-
-    print("Close the client socket")
-    writer.close()
 
 
 @asyncio.coroutine
-def pipe(reader, writer, message):
-    while not reader.at_eof():
-        header = yield from reader.read(12)
-        if not header:
-            break
-        print(message, ": Row header:", header)
-        assert len(header) == 12
-        magic = header[:4].decode()
-        print(message, ": magic :", magic)
-        assert magic == "GIOP"
-        version = header[4], header[5]
-        print(message, ": version :", version)
-        assert version in [(1, 0), (1, 1)]
-        order = "<" if header[6] else ">"
-        print(message, ": order :", order)
-        message_type = MESSAGE_TYPE[header[7]]
-        print(message, ": message_type :", message_type)
-        size = struct.unpack(order + "L", header[8:12])[0]
-        print(message, ": size :", size)
-
-        data = yield from reader.read(size)
-        if message_type == "Reply":
-            body_header = data[0:12]
-            service_context = struct.unpack(order + "L", body_header[0:4])[0]
-            request_id = struct.unpack(order + "L", body_header[4:8])[0]
-            reply_status = struct.unpack(order + "L", body_header[8:12])[0]
-            print(message, ": body header : service_context :", service_context)
-            print(message, ": body header : request_id :", request_id)
-            print(message, ": body header : reply_status :", reply_status)
-            body = data[12:]
-            for i in range(0, len(body), 8):
-                print(message, ": body :", body[i:i+4], body[i+4:i+8])
-            if IOR_TOKEN in body:
-                index = [i for i in range(len(body)) if
-                         body[i:i + len(IOR_TOKEN)] == IOR_TOKEN][0]
-                ior_size = struct.unpack(order + "L", body[index - 4:index])[0]
-                ior = body[index:index + ior_size - 1]
-                res = ior_decoder.unmarshal(ior)
-                #host, port = res
-
-
-                host = res.host[:-1].decode()
-                port = int(res.port)
-
-                server = yield from  asyncio.start_server(partial(subscribed_handler, host , port ), '0.0.0.0', 0, loop=loop)
-                socket_port = server.sockets[0].getsockname()[1]
-
-                d = res._asdict()
-                d["port"] = socket_port
-                d["host"] = NETWORK_HOST.encode() + b'\x00'
-                ior = ior_decoder.IOR(**d)
-                ior = ior_decoder.marshal(ior) + b"\x00"
-                patch = struct.pack("I{}s0I".format(len(ior)), len(ior), ior)
-                stop = index + struct.calcsize("{}s0I".format(ior_size))
-                start = index-4
-                new_body = body[:start] + patch + body[stop:]
-                new_size = size - len(body) + len(new_body)
-                data = data[:12] + new_body
-                header = header[:8] + struct.pack("I", new_size)
-                foo = header + data
-                #for i in range(0, len(foo), 8):
-                #    print(message, ":", i, "--", foo[i:i + 4], foo[i + 4:i + 8])
-                for i in range(0, len(body), 4):
-                    print(message, ":", i, "--", body[i:i + 4], new_body[i:i +4], body[i:i + 4] ==  new_body[i:i +4])
-
-        else:
-            print(message, ": body :", data)
-        writer.write(header + data)
-    writer.close()
+def inspect_pipe(reader, writer):
+    with closing(writer):
+        while not reader.at_eof():
+            data = yield from read_frame(reader)
+            if not data:
+                break
+            writer.write(data)
 
 
 @asyncio.coroutine
-def handle_request(server_reader, server_writer):
+def read_frame(reader):
+    # Read header
+    loop = reader._loop
+    raw_header = yield from reader.read(12)
+    if not raw_header:
+        return raw_header
+    header = giop.unpack_giop_header(raw_header)
+    # Read data
+    raw_data = yield from reader.read(header.size)
+    raw_frame = raw_header + raw_data
+    if message_type != giop.MessageType.Reply:
+        return raw_frame
+    # Unpack reply
+    raw_reply_header, raw_body = raw_data[:12], raw_data[12:]
+    repy_header = giop.unpack_reply_header(raw_reply_header)
+    if reply_header.reply_status != giop.ReplyStatus.NoException:
+        return raw_frame
+    # Find IOR, host and port
+    ior = giop.find_ior(raw_body)
+    if not ior:
+        return raw_frame
+    ior, start, stop = ior
+    host = ior.host[:-1].decode()
+    key = host, ior.port
+    # Start port forwarding
+    if key not in loop.forward_dict:
+        args = partial(forward, host=host, port=ior.port), loop.bind_address, 0
+        server = yield from asyncio.start_server(*args, loop=loop)
+        value = (server.sockets[0].getsockname()[1],
+                 server.sockets[0].getsockname()[0].encode() + b'\x00')
+        loop.forward_dict[key] = value
+        print("Forwarding {} to {}...".format(value, key))
+    # Patch IOR
+    host, port = loop.forward_dict[key]
+    ior = ior._replace(host=host, port=port)
+    # Repack body
+    raw_body = giop.repack_ior(raw_body, ior, start, stop)
+    raw_data = raw_reply_header + raw_body
+    header = header._replace(size=len(raw_data))
+    return giop.pack_giop(header, raw_body)
+
+
+@asyncio.coroutine
+def inspect(server_reader, server_writer):
     client_reader, client_writer = yield from asyncio.open_connection(
         *TANGO_HOST.split(":"), loop=loop)
-    task1 = pipe(server_reader, client_writer, "client to db")
-    task2 = pipe(client_reader, server_writer, "db to client")
+    task1 = inspect_pipe(server_reader, client_writer)
+    task2 = inspect_pipe(client_reader, server_writer)
     yield from asyncio.gather(task1, task2)
-    print("Close ok")
 
 
-loop = asyncio.get_event_loop()
-loop.set_debug(True)
-coro = asyncio.start_server(handle_request, '0.0.0.0', 8888, loop=loop)
-server = loop.run_until_complete(coro)
-
-# Serve requests until Ctrl+C is pressed
-print('Serving on {}'.format(server.sockets[0].getsockname()))
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
-
-# Close the server
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+def run_server(bind_address, server_port):
+    # Initialize loop
+    loop = asyncio.get_event_loop()
+    loop.bind_address = bind_address
+    loop.server_port = server_port
+    loop.forward_dict = {}
+    # Create server
+    coro = asyncio.start_server(inspect, bind_address, server_port)
+    server = loop.run_until_complete(coro)
+    # Serve requests until Ctrl+C is pressed
+    print('Serving on {}'.format(server.sockets[0].getsockname()))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    # Close the server
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()

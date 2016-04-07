056f9c96b6e62a170fba23fbe0c6b49512a41b21
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-04-07 17:27:34 +0200

    Default to port 10000

--- tangogateway/gateway.py
+++ tangogateway/gateway.py
@@ -392,8 +392,8 @@ def main(*args):
         '--bind', '-b', metavar='ADDRESS', default='0.0.0.0',
         help='Specify the bind address (default is all interfaces)')
     parser.add_argument(
-        '--port', '-p', metavar='PORT', default=8000, type=int,
-        help='Port for the server (default is 8000)')
+        '--port', '-p', metavar='PORT', default=10000, type=int,
+        help='Port for the server (default is 10000)')
     parser.add_argument(
         '--tango', '-t', metavar='HOST',
         help='Tango host (default is given by PyTango)')

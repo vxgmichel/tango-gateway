2e3bcf43267205cc748291a3c23f467ecafee5a1
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-11-28 14:16:40 +0100

    Bump version 0.2.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def safe_read(fname):
 
 # Setup
 setup(name="tango-gateway",
-      version="0.2.1",
+      version="0.2.2",
       packages=['tangogateway'],
       entry_points={
           'console_scripts': ['tango-gateway = tangogateway:main']},
